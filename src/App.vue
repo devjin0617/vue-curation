55049f17211aa882cdba0038a23a3f1b8c46b186
devjin0617 <devjin0617@gmail.com>
2017-12-29 00:39:36 +0900

    상단 메뉴 추가

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,21 @@
       <el-header>
         <el-row :gutter="20">
           <el-col :span="3">Vue Curation</el-col>
-          <el-col :span="18">XRP</el-col>
+          <el-col :span="18">
+            <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect"
+              background-color="#222222"
+              text-color="#FEFEFE"
+              active-text-color="#31A2D8">
+              <el-menu-item index="1">Home</el-menu-item>
+              <el-submenu index="2">
+                <template slot="title">Collection</template>
+                <el-menu-item index="2-1">item one</el-menu-item>
+                <el-menu-item index="2-2">item two</el-menu-item>
+                <el-menu-item index="2-3">item three</el-menu-item>
+              </el-submenu>
+              <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">Archive</a></el-menu-item>
+            </el-menu>
+          </el-col>
           <el-col :span="3">
             <el-button size="small">Login</el-button>
           </el-col>
