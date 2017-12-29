ef289fd64e0ac732139b81a9269feb678f96346b
devjin0617 <devjin0617@gmail.com>
2017-12-29 22:58:35 +0900

    vuefire 추가

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-theme-chalk'
 
+Vue.use(VueFire)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
